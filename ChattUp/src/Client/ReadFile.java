7cea9e634e38e145ad1167ecdfe98bfe5b4d5ee8
Elsa Schröder <elsa.schroder@gmail.com>
2016-05-12 09:45:42 +0200

    La till den borttagna filen igen

--- ChattUp/src/Client/ReadFile.java
+++ ChattUp/src/Client/ReadFile.java
@@ -1,47 +1,45 @@
+package Client;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
+public class ReadFile extends Thread {
 
-	package Client;
-	import java.io.BufferedReader;
-	import java.io.IOException;
-	import java.io.InputStreamReader;
-	import java.net.Socket;
+	private Socket socket;
 
-	public class ReadFile extends Thread {
+	public ReadFile(Socket socket) {
+		this.socket = socket;
+	}
 
-		private Socket socket;
+	public void run() {
+		boolean isOnline = true;
+		try {
 
-		public ReadFile(Socket socket) {
-			this.socket = socket;
+			while (isOnline) {
+
+				InputStreamReader in = new InputStreamReader(
+						socket.getInputStream());
+				BufferedReader buffer = new BufferedReader(in);
+				String answer = buffer.readLine();
+				if (answer.endsWith("Q")) {
+					isOnline = false;
+				} else {
+					System.out.println(answer);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
-		public void run() {
-			boolean isOnline = true;
-			try {
-
-				while (isOnline) {
-
-					InputStreamReader in = new InputStreamReader(
-							socket.getInputStream());
-					BufferedReader buffer = new BufferedReader(in);
-					String answer = buffer.readLine();
-					if (answer.endsWith("Q")) {
-						isOnline = false;
-					} else {
-						System.out.println(answer);
-					}
-				}
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			try {
-				socket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
+		try {
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}
+
+}
 
