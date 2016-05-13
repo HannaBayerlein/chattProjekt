14cd38f1fcafaac56dbadb8b10c5bbfa6b1583f8
elinhanstorp <elin.hanstorp@live.com>
2016-05-13 09:04:27 +0200

    flyttade alla klasser från default till ett server-paket så man kan nå det från GUI:t. I default ligger det ingenstans och kan därmed inte anropas

---
+++ ChattApp/src/server/Session.java
@@ -0,0 +1,58 @@
+package server;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Session extends Thread {
+
+	private Scanner input;
+	private String message = "";
+	private MailBox mailBox;
+	private User user;
+	private ArrayList<User> activeUsers;
+
+	public Session(MailBox mailBox, User user, ArrayList<User> activeUsers) {
+		this.mailBox = mailBox;
+		this.user = user;
+		this.activeUsers = activeUsers;
+	}
+
+	public void run() {
+
+		try {
+
+			input = new Scanner(user.getSocket().getInputStream());
+			while (true) {
+
+				message = input.nextLine();
+
+				try {
+					if (activeUsers.contains(user)) {
+						if (message.startsWith("N:")) {
+							String changeNick = message.substring(3,
+									message.length());
+							user.setNick(changeNick);
+
+						} else if (message.startsWith("Q")) {
+							String exitMessage = "valde att lämna chatten";
+							mailBox.add(user, exitMessage);
+							activeUsers.remove(user);
+
+						} else {
+							mailBox.add(user, message);
+						}
+					}
+
+				} catch (InterruptedException e) {
+
+					e.printStackTrace();
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
